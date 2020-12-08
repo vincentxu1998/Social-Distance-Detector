c3063ad181226484589d9e26e6e4b1a221a9856e
VincentXu <vincentxu1998@outlook.com>
2020-12-08 11:35:18 -0500

    Commit Structure

--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
 import os
 import tarfile
 import wget
-
 from pycocotools.coco import COCO
 import requests
+
+from torch.utils.data import Dataset, DataLoader
 
 def download_dataset(path='./input/coco_person'):
     if not os.path.exists(path):
@@ -21,6 +22,44 @@ def download_dataset(path='./input/coco_person'):
             img_data = requests.get(im['coco_url']).content
             with open(os.path.join(path, im['file_name']), 'wb') as handler:
                 handler.write(img_data)
+
+# Dataset helper function
+def read_image(path):
+    return None
+
+##################Data Loader #######3
+class CUB(Dataset):
+    def __init__(self, files_path, split, train=True):
+        return
+    def __len__(self):
+        return self.num_files
+
+    def __getitem__(self, index):
+        return
+
+def initialize_loader(train_batch_size=64, val_batch_size=64):
+    return
+
+def visualize_dataset(dataloader):
+    return
+
+def plot_prediction(args, model, is_train, index_list=[0], plotpath=None, title=None):
+    return
+
+##############helper function for training##############
+def compute_loss(pred, gt):
+    return
+
+def run_validation_step(args, epoch, model, loader, plotpath=None):
+    return
+
+def train(args, model):
+    return
+
+class AttrDict(dict):
+    def __init__(self, *args, **kwargs):
+        super(AttrDict, self).__init__(*args, **kwargs)
+        self.__dict__ = self
 
 
 if __name__ == '__main__':
