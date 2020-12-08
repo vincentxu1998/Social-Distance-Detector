8be8a30f77534f27dd99bfc64af5ae16a73027bb
Jack <qq835490396@gmail.com>
2020-12-08 16:06:13 -0500

    Added initial model skeleton

--- src/main.py
+++ src/main.py
@@ -18,6 +18,11 @@ from torch.utils.data import Dataset, DataLoader
 import numpy as np
 import src.transforms as T
 
+import torchvision
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+from torchvision.models.detection.mask_rcnn import MaskRCNNPredictor
+
+
 
 def download_dataset(path='./input/coco_person'):
     if not os.path.exists(path):
@@ -108,6 +113,25 @@ class CUB(Dataset):
             img, target = self.transforms(img, target)
 
         return img, target
+
+def get_model_instance_segmentation(num_classes):
+    # load an instance segmentation model pre-trained pre-trained on COCO
+    model = torchvision.models.detection.maskrcnn_resnet50_fpn(pretrained=True)
+
+    # get number of input features for the classifier
+    in_features = model.roi_heads.box_predictor.cls_score.in_features
+    # replace the pre-trained head with a new one
+    model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+
+    # now get the number of input features for the mask classifier
+    in_features_mask = model.roi_heads.mask_predictor.conv5_mask.in_channels
+    hidden_layer = 256
+    # and replace the mask predictor with a new one
+    model.roi_heads.mask_predictor = MaskRCNNPredictor(in_features_mask,
+                                                       hidden_layer,
+                                                       num_classes)
+
+    return model
 
 def initialize_loader(train_batch_size=4, val_batch_size=4):
     train_dataset = CUB(PennFudanPath, get_transform(train=True))
